c0b67e230cab984132d5426c02f80a5002e81d88
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2022-11-14 15:06:21 +0600

    services section

--- src/Pages/Home/Services/Service.js
+++ src/Pages/Home/Services/Service.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { name, description, img } = service;
     return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+        <div className="card card-compact w-full bg-base-100 shadow-xl py-10 mt-10">
+            <figure><img src={img} alt={img} /></figure>
             <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
+                <h2 className="text-2xl font-bold">{name}</h2>
+                <p className='text-lg'>{description}</p>
             </div>
         </div>
     );
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import cavity from '../../../assets/images/cavity.png'
+import fluoride from '../../../assets/images/fluoride.png'
+import whitening from '../../../assets/images/whitening.png'
+import Service from './Service';
 
 const Services = () => {
+    const services = [
+        {
+            id: 1,
+            name: 'Cavity Treatment',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: cavity
+        },
+        {
+            id: 2,
+            name: 'Fluoride Treatment',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: fluoride
+        },
+        {
+            id: 3,
+            name: 'Whitening Treatment',
+            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the',
+            img: whitening
+        },
+    ]
     return (
-        <div>
-
+        <div className='text-center mt-32'>
+            <div className='mb-10'>
+                <h3 className='text-primary text-3xl font-bold mb-5'>Our Services</h3>
+                <h1 className="text-5xl">Services We Provide</h1>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
