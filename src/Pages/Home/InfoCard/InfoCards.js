d133ef384b997c126cf112933411b9b44f7da140
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2022-11-14 14:28:13 +0600

    navbar, banner design

---
+++ src/Pages/Home/InfoCard/InfoCards.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import clock from '../../../assets/icons/clock.svg'
+import marker from '../../../assets/icons/marker.svg'
+import phone from '../../../assets/icons/phone.svg'
+
+const InfoCards = () => {
+    const info = [
+        {
+            id: 1,
+            icon: clock
+        }
+    ]
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default InfoCards;
