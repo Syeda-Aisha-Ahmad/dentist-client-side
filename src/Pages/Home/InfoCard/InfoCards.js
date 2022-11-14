eb8f89bfb96b64fa3ba0fd5f29c69778fed62dbe
Syeda-Aisha-Ahmad <18aishaahmad@gmail.com>
2022-11-14 14:48:09 +0600

    info cards added home page

--- src/Pages/Home/InfoCard/InfoCards.js
+++ src/Pages/Home/InfoCard/InfoCards.js
@@ -2,17 +2,40 @@ import React from 'react';
 import clock from '../../../assets/icons/clock.svg'
 import marker from '../../../assets/icons/marker.svg'
 import phone from '../../../assets/icons/phone.svg'
+import InfoCard from './InfoCard';
 
 const InfoCards = () => {
-    const info = [
+    const infos = [
         {
             id: 1,
-            icon: clock
+            icon: clock,
+            description: 'Open 9.00 am to 5.00 pm everyday',
+            name: 'Opening Hours',
+            bgClass: 'bg-primary'
+        },
+        {
+            id: 2,
+            icon: marker,
+            description: 'Open 9.00 am to 5.00 pm everyday',
+            name: 'Opening Hours',
+            bgClass: 'bg-accent'
+        },
+        {
+            id: 3,
+            icon: phone,
+            description: 'Open 9.00 am to 5.00 pm everyday',
+            name: 'Opening Hours',
+            bgClass: 'bg-primary'
         }
     ]
     return (
-        <div>
-
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            {
+                infos.map(info => <InfoCard
+                    key={info.id}
+                    info={info}
+                ></InfoCard>)
+            }
         </div>
     );
 };
